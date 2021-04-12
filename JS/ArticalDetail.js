4bef3e329fe9fa4a22c3e5e6d90b1c4770abc960
zora258873 <zora258873@gmail.com>
2021-04-12 10:27:43 +0800

    upload

--- JS/ArticalDetail.js
+++ JS/ArticalDetail.js
@@ -1,15 +1,15 @@
 // Nav滾動陰影
-function scrollshadow() { 
+function scrollshadow() {
   var topShadow = document.getElementById("top-shadow");
   var y = document.documentElement.scrollTop;
   var nav = document.getElementById("nav");
   if (y > 40) {
     topShadow.style.display = 'block';
-    topShadow.style.opacity =  0.5;
+    topShadow.style.opacity = 0.5;
     nav.style.backgroundColor = 'white';
   } else {
     topShadow.style.display = 'none';
-    topShadow.style.opacity =  0.5;
+    topShadow.style.opacity = 0.5;
     nav.style.backgroundColor = '#fdf1cf';
   }
 };
@@ -17,13 +17,18 @@ window.addEventListener('scroll', scrollshadow, false);
 
 
 //切換圖片
-function showLarge(e){;
+function showLarge(e) {
+  ;
   document.getElementById("large").src = e.target.src;
 }
-function init(){
-  let smalls = document.getElementsByClassName("small");
-  for(let i=0; i<smalls.length; i++){
+function init() {
+  let smalls = document.querySelectorAll(".small");
+  console.log(smalls);
+
+  smalls.forEach(e => e.classList.remove('active'))
+  for (let i = 0; i < smalls.length; i++) {
     smalls[i].onclick = showLarge;
+    smalls[i].classList.add('active')
   }
 }
 window.onload = init;
